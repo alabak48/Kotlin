5ff0047933f6001359330782e4e79e690d5cf12d
Anja Labak <anja.labak12345@gmail.com>
2022-02-18 21:52:27 +0100

    Bacanje kocke

--- app/src/main/java/com/anja/diceroller/MainActivity.kt
+++ app/src/main/java/com/anja/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.anja.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +12,20 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView : TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+private class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return(1..numSides).random()
     }
 }
