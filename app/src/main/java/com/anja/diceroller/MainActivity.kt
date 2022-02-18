ee314230920a71eae323d4d19f39ac7afc372481
Anja Labak <anja.labak12345@gmail.com>
2022-02-18 22:04:28 +0100

    Dodavanje komentara

--- app/src/main/java/com/anja/diceroller/MainActivity.kt
+++ app/src/main/java/com/anja/diceroller/MainActivity.kt
@@ -5,27 +5,39 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
+/**
+ * Ova aktivnost omogućuje korisniku da baci kocku i pogleda rezultat
+ * na ekranu.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Bacite kocku i ažurirajte zaslon s rezultatom.
+     */
     private fun rollDice() {
+        // Kreiranje novog Dice objekta sa 6 strana s funckijom bacanja kocke.
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView : TextView = findViewById(R.id.textView)
+        // Ažurirajte zaslon s bacanjem kocke.
+        val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
+/**
+ * Kreiranje klase Dice s brojčanim podacima. Klasa Dice s metodom roll
+ * koja vraća/generira nasumičan broj od 1 do 6 koji se nalaze u varijabli numSides.
+ */
 private class Dice(val numSides: Int) {
     fun roll(): Int {
-        return(1..numSides).random()
+        return (1..numSides).random()
     }
 }
