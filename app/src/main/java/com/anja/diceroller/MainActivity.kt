e3fc006cb02c4979ea04a44b7c6c25b63723a2ac
Anja Labak <anja.labak12345@gmail.com>
2022-02-27 19:15:04 +0100

    Dodavanje slika svih mogućih ishoda kocke

--- app/src/main/java/com/anja/diceroller/MainActivity.kt
+++ app/src/main/java/com/anja/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.anja.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -23,14 +23,20 @@ class MainActivity : AppCompatActivity() {
      * Bacite kocku i ažurirajte zaslon s rezultatom.
      */
     private fun rollDice() {
-        // Kreiranje novog Dice objekta sa 6 strana s funckijom bacanja kocke.
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // Ažurirajte zaslon s bacanjem kocke.
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+       when (diceRoll) {
+           1 -> diceImage.setImageResource(R.drawable.dice_1)
+           2 -> diceImage.setImageResource(R.drawable.dice_2)
+           3 -> diceImage.setImageResource(R.drawable.dice_3)
+           4 -> diceImage.setImageResource(R.drawable.dice_4)
+           5 -> diceImage.setImageResource(R.drawable.dice_5)
+           6 -> diceImage.setImageResource(R.drawable.dice_6)
+       }
     }
-}
 
 /**
  * Kreiranje klase Dice s brojčanim podacima. Klasa Dice s metodom roll
@@ -40,4 +46,4 @@ private class Dice(val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
-}
+}}
